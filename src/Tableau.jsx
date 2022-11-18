2eee68905c137fd37c65041886c7d7c3512bb482
Wilson <chwchiu@connect.ust.hk>
2022-11-18 23:30:35 +0800

    chore: refactor

--- src/Tableau.jsx
+++ src/Tableau.jsx
@@ -3,7 +3,7 @@ const { tableau } = window;
 
 export const Tableau = () => {
     const ref = useRef(null)
-    const url = "http://public.tableau.com/views/LearnEmbeddedAnalytics/SalesOverviewDashboard"
+    const url = "https://prod-apnortheast-a.online.tableau.com/t/4462dataviz/views/OpeningHoursAnalysisHeatmap/Dashboard1?:origin=card_share_link&:embed=n"
     function initViz() {
         new tableau.Viz(ref.current, url)
     }
@@ -14,7 +14,7 @@ export const Tableau = () => {
     }, [])
 
     return (
-        <div>
+        <div style={{ width: "50vw", height: "50vh", }}>
             <p>This is a tableau embed</p>
             <div ref={ref}></div>
         </div>
