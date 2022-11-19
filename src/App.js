1be8595afaf312bf7d62b80768f40323a414111f
Nathan Hui <nathanhui@cryptoblk.io>
2022-11-19 13:01:01 +0800

    feat: adding card preview on click

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import "./App.css";
 import Tableau from "./visualizations/Tableau";
-import { Tab, Tabs } from "react-bootstrap";
+import { Tab, Tabs, Row, Col, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const heatmapURL =
+    "https://prod-apnortheast-a.online.tableau.com/t/4462dataviz/views/OpeningHoursAnalysisHeatmapSmallMultiples/Dashboard1";
+  const mapURL =
+    "https://prod-apnortheast-a.online.tableau.com/t/4462dataviz/views/BusinessesInPennysylvania/Dashboard1";
+
   return (
     <div className="App">
-      <h1>Tableau Dashboard in React </h1>
-      <Tabs
-        defaultActiveKey="profile"
-        id="uncontrolled-tab-example"
-        className="mb-3"
-      >
-        <Tab eventKey="home" title="Home">
-          <Tableau />
-        </Tab>
-        <Tab eventKey="profile" title="Profile">
-          <Tableau />
-        </Tab>
-        <Tab eventKey="contact" title="Contact" disabled>
-          <Tableau />
-        </Tab>
-      </Tabs>
+      <div className="row">
+        <div className="col-6">
+          <Tableau url={mapURL} />
+        </div>
+        <div className="col-6">
+          <Tableau url={heatmapURL} />
+        </div>
+      </div>
     </div>
   );
 }
